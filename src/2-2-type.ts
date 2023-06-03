39a2348c81423f55ecd55480919bb54db219f62c
songmin <tft0720@gmail.com>
2023-06-03 17:21:21 +0900

    타입 단언

--- src/2-2-type.ts
+++ src/2-2-type.ts
@@ -16,5 +16,34 @@ function func(a: number, b?: number, c?: number) {
   }
   console.log(a * 20);
 }
-console.log(func(1, 2, 3));
-console.log(func(1)); // 구현부의 파라미터를 따르는게 아니라 선언부를 확인함
+func(1, 2, 3);
+func(1); // 구현부의 파라미터를 따르는게 아니라 선언부를 확인함
+
+/**
+ * * 사용자 정의 타입 가드
+ */
+
+type Dog = {
+  name: string;
+  isBark: boolean;
+};
+
+type Cat = {
+  name: string;
+  isScratch: boolean;
+};
+// 서로소 유니언타입을 사용할 수 없다고 가정한다
+// 타입스크립트에게 반환값으로 알수 있도록 명시해준다.
+function isDog(animal: Animal): animal is Dog {
+  // <- animal Dog 타입이라면(true)
+  return (animal as Dog).isBark !== undefined;
+}
+
+type Animal = Dog | Cat;
+function warning(animal: Animal) {
+  if (isDog(animal)) {
+    console.log(animal.isBark); // isBark 접근 가능
+  } else {
+    console.log(animal.isScratch);
+  }
+}
